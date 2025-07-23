b9c150931bbfd059738c12a323598fa1befe0bc4
bigdreamdeveloper <mustafaebee@gmail.com>
2025-07-23 10:25:44 +0800

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,36 @@
-// localStorage functions
+// storage.js
+
+function savePlayerData(player) {
+  localStorage.setItem("playerData", JSON.stringify(player));
+}
+
+function loadPlayerData() {
+  const data = localStorage.getItem("playerData");
+  if (!data) return null;
+  return JSON.parse(data);
+}
+
+function resetPlayerData() {
+  localStorage.removeItem("playerData");
+}
+
+// Untuk digunakan bila player mula game baru
+function initNewPlayer(name, sprite) {
+  const player = {
+    name: name,
+    sprite: sprite,
+    level: 1,
+    exp: 0,
+    stats: {
+      strength: 1,
+      intel: 1,
+      health: 5,
+      luck: 1,
+      agility: 1
+    },
+    unlockedMap: 1,
+    inventory: [],
+  };
+  savePlayerData(player);
+  return player;
+}
